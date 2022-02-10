9567ea843396bde5761ac02e24490c224e937adb
Rades Nurgaliev <lightgreen.spring@gmail.com>
2022-02-10 09:00:43 +0300

    Added applying a promocode, +refactoring

--- src/Controller/PromocodeController.php
+++ src/Controller/PromocodeController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Promocode;
+use App\Repository\PromocodeRepository;
 use App\Services\KeywordService;
 use App\Validator\ComplexValidator;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,7 +18,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PromocodeController extends AbstractController
 {
 
-    private EntityManager $entityManager;
+    private ObjectManager $entityManager;
 
     public function __construct(ManagerRegistry $doctrine)
     {
@@ -33,40 +36,67 @@ class PromocodeController extends AbstractController
     #[Route('/generate', name: 'generate', methods: ['GET'])]
     public function generate(Request $request): Response
     {
-
         $validator = new ComplexValidator();
-        $validator->validate($request);
 
-        if (!$validator->isPassed()) {
-            return $this->json([
-                'status' => 'error',
-                'data' => [],
-                'errors' => $validator->getErrors()
-            ]);
+        if (!$validator->validateGeneration($request)->isPassed()) {
+            return $this->prepareResponse('error', errors: $validator->getErrors());
         }
 
-        $discountPercent = $request->get('discount_percent');
-        $numberOfUses = $request->get('number_of_uses');
         $keyword = KeywordService::createNewKeyword();
-
         try {
             $promocode = new Promocode();
-            $promocode->setDiscountPercent($discountPercent);
-            $promocode->setNumberOfUses($numberOfUses);
+            $promocode->setDiscountPercent($request->get('discount_percent'));
+            $promocode->setNumberOfUses($numberOfUses = $request->get('number_of_uses'));
             $promocode->setKeyword($keyword);
             $this->entityManager->persist($promocode);
             $this->entityManager->flush();
         } catch (\Throwable $exception) {
-            return $this->json([
-                'status' => 'error',
-                'data' => [],
-                'errors' => [$exception->getMessage()]
-            ]);
+            return $this->prepareResponse('error', errors: [$exception->getMessage()]);
         }
-        return $this->json([
-            'status' => 'success',
-            'data' => $keyword,
-            'errors' => []
-        ]);
+        return $this->prepareResponse('success', ['keyword' => $keyword]);
+    }
+
+    #[Route('/apply', name: 'apply', methods: ['GET'])]
+    public function apply(Request $request): Response
+    {
+        $validator = new ComplexValidator();
+        if (!$validator->validateApplication($request)->isPassed()) {
+            return $this->prepareResponse('error', errors: $validator->getErrors());
+        }
+
+
+        /** @var Promocode $promocode */
+        $promocode = ($this->entityManager->getRepository(Promocode::class))
+            ->findOneBy(['keyword' => $request->get('keyword')]);
+        if ($promocode === null) {
+            return $this->prepareResponse('error', errors: ['Promocode does not exist!']);
+        }
+        if ($promocode->getNumberOfUses() === 0) {
+            return $this->prepareResponse(
+                'error',
+                errors: ['The number of uses of the promocode has been exhausted!']
+            );
+        }
+
+        $this->entityManager->beginTransaction();
+        try {
+            $promocode->setNumberOfUses($promocode->getNumberOfUses() - 1);
+            $this->entityManager->persist($promocode);
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+        } catch (\Throwable $exception) {
+            $this->entityManager->rollback();
+            return $this->prepareResponse('error', errors: [$exception->getMessage()]);
+        }
+
+        return $this->prepareResponse(
+            'success',
+            ['number_of_uses' => $promocode->getNumberOfUses(), 'discount_percent' => $promocode->getDiscountPercent()]
+        );
+    }
+
+    private function prepareResponse(string $status, array $data = [], array $errors = []): Response
+    {
+        return $this->json(['status' => $status, 'data' => $data, 'errors' => $errors]);
     }
 }
--- src/Validator/ComplexValidator.php
+++ src/Validator/ComplexValidator.php
@@ -4,6 +4,7 @@ namespace App\Validator;
 
 use Symfony\Component\HttpFoundation\Request;
 
+//@TODO It would be better to use validator by Symfony
 class ComplexValidator
 {
     private array $errors = [];
@@ -19,22 +20,36 @@ class ComplexValidator
         return $this->passed;
     }
 
-    public function validate(Request $request)
+    public function validateGeneration(Request $request): self
     {
-        //@TODO It would be better to use validator by Symfony
         $discountPercent = $request->get('discount_percent');
         $numberOfUses = $request->get('number_of_uses');
         if ($discountPercent === null) {
             $this->errors[] = 'discount_percent value must be set!';
+            $this->passed = false;
         }
         if ($numberOfUses === null) {
             $this->errors[] = 'number_of_uses value must be set!';
+            $this->passed = false;
         }
         if (!is_numeric($discountPercent)) {
             $this->errors[] = 'discount_percent value must have a float type!';
+            $this->passed = false;
         }
         if (!is_numeric($numberOfUses)) {
             $this->errors[] = 'number_of_uses value must have a numeric type!';
+            $this->passed = false;
         }
+        return $this;
+    }
+
+    public function validateApplication(Request $request): self
+    {
+        $keyword = $request->get('keyword');
+        if ($keyword === null) {
+            $this->errors[] = 'keyword value must must be set!';
+            $this->passed = false;
+        }
+        return $this;
     }
 }
