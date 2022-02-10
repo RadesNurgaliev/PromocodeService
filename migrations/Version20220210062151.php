5f800b06d0626790d6de80c181464d560ebe783e
Rades Nurgaliev <lightgreen.spring@gmail.com>
2022-02-10 09:23:11 +0300

    Added index to keyword, magration rewritten

---
+++ migrations/Version20220210062151.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220210062151 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE promocode (id INT NOT NULL, keyword VARCHAR(255) NOT NULL, discount_percent DOUBLE PRECISION NOT NULL, number_of_uses INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX keyword_uniq_idx ON promocode (keyword)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE promocode');
+    }
+}
--- src/Entity/Promocode.php
+++ src/Entity/Promocode.php
@@ -6,6 +6,7 @@ use App\Repository\PromocodeRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PromocodeRepository::class)]
+#[ORM\UniqueConstraint(name: 'keyword_uniq_idx', columns: ['keyword'])]
 class Promocode
 {
     #[ORM\Id]
